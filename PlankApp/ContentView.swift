5a1da7416d1c8364258648dde22c598c7eb8d79b
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-05-13 20:38:05 +0200

    Initial Commit

---
+++ PlankApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlankApp
+//
+//  Created by Ivan Trajanovski on 13.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlankApp/PlankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlankAppApp.swift
+//  PlankApp
+//
+//  Created by Ivan Trajanovski on 13.05.24.
+//
+
+import SwiftUI
+
+@main
+struct PlankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
